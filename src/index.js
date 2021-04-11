55215fd071ae3df11efceb72b1477d693adbc837
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-04-11 18:53:53 -0300

    fundamentos node

--- src/index.js
+++ src/index.js
@@ -3,36 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello world!" });
-});
-
-app.get("/courses", (request, response) => {
-  const query = request.query;
-  console.log(query);
-  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
-});
-
-app.post("/courses", (request, response) => {
-  const body = request.body;
-  console.log(body);
-  return response.json(["Curso 1", "curso 2", "curso 3", "curso 4"]);
-});
-
-app.put("/courses/:id", (request, response) => {
-  const params = request.params;
-  console.log(params);
-  return response.json(["Curso 6", "curso 2", "curso 3", "curso 4"]);
-});
-
-app.patch("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "curso 7", "curso 3", "curso 4"]);
-});
-
-app.delete("/courses/:id", (request, response) => {
-  return response.json(["curso 3", "curso 4"]);
-});
-
 app.listen(3333, () => {
   console.log("Servido rodando 🚀");
 });
